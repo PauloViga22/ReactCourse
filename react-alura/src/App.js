53e1fa9b9bae980030e77822f4b6835f1f0ba6a3
PAULO VICTOR DE CASTRO VIGA <paulo.22.5@gmail.com>
2019-08-05 13:23:17 -0700

    Adding State to the pro

--- react-alura/src/App.js
+++ react-alura/src/App.js
@@ -1,37 +1,39 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Tabela from './Tabela';
+import Tabela from "./Tabela";
 
 class App extends Component {
-  
-    autores =  [
+  state = {
+    autores: [
       {
-        nome: 'Paulo',
-        livro: 'React',
-        preco: '1000'
+        nome: "Paulo",
+        livro: "React",
+        preco: "1000"
       },
       {
-        nome: 'Daniel',
-        livro: 'Java',
-        preco: '99'
+        nome: "Daniel",
+        livro: "Java",
+        preco: "99"
       },
       {
-        nome: 'Marcos',
-        livro: 'Design',
-        preco: '150'
+        nome: "Marcos",
+        livro: "Design",
+        preco: "150"
       },
       {
-        nome: 'Bruno',
-        livro: 'DevOps',
-        preco: '100'
+        nome: "Bruno",
+        livro: "DevOps",
+        preco: "100"
       }
-    ];
-
+    ]
+  };
 
   render() {
     return (
-      <Tabela autores={this.autores}/>  
+      <div className="App">
+        <Tabela autores={this.state.autores} />
+      </div>
     );
   }
 }

ea15622a7a28bde732b2785f9019f8bcf4aa4ad9
PAULO VICTOR DE CASTRO VIGA <paulo.22.5@gmail.com>
2019-08-05 14:07:57 -0700

    Add Delete author method

--- react-alura/src/App.js
+++ react-alura/src/App.js
@@ -29,10 +29,20 @@ class App extends Component {
     ]
   };
 
+  removeAutor = index => {
+    const { autores } = this.state;
+
+    this.setState({
+      autores: autores.filter((autor, current) => {
+        return current !== index;
+      })
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <Tabela autores={this.state.autores} />
+        <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
       </div>
     );
   }
